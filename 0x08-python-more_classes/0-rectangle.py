4bdf488e49a6e9d0e2a8733beaf47f7e62d97d18
G.A.N <56300754+GAN-007@users.noreply.github.com>
2022-10-18 16:49:22 +0300

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -6,7 +6,6 @@ class Rectangle:
     pass
 
 
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
+class Rectangle:
+    """Empty representation of a Rectangle"""
+    pass
